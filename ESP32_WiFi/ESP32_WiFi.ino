dfe98d38d2ceac210f51c383b1cac8a4238b758a
robocodebr <robocodebr@gmail.com>
2020-11-19 03:31:44 +0200

    project file

--- ESP32_WiFi/ESP32_WiFi.ino
+++ ESP32_WiFi/ESP32_WiFi.ino
@@ -1,35 +1,3 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  This example runs directly on ESP32 chip.
-
-  Note: This requires ESP32 support package:
-    https://github.com/espressif/arduino-esp32
-
-  Please be sure to select the right ESP32 module
-  in the Tools -> Board menu!
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
@@ -38,8 +6,8 @@
 #include <DHT.h>
 
 char auth[] = "OkwJfDQHGzAYt8UxCZRi7hMC5QPSV8vy";
-char ssid[] = "kv-41";
-char pass[] = "greenmoon1558";
+char ssid[] = "wifi";
+char pass[] = "password";
 
 #define DHTPIN 27          
 #define DHTTYPE DHT11     // DHT 11
@@ -47,22 +15,7 @@ char pass[] = "greenmoon1558";
 DHT dht(DHTPIN, DHTTYPE);
 BlynkTimer timer;
 
-void sendSensor()
-{
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
-
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  Serial.print("Humidity: ");
-  Serial.println(h);
-  Serial.print("%  Temperature: ");
-  Serial.println(t);
-
-  Blynk.virtualWrite(V5, h);
-  Blynk.virtualWrite(V6, t);
+Blynk.virtualWrite(V6, t);
 }
 
 void setup()
